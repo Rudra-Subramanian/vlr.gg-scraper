de14817c629a864846fa675c8f52f16958a0b540
NYSBC-Rudra <133771854+NYSBC-Rudra@users.noreply.github.com>
2025-03-06 14:11:49 -0500

    adding an api test and changing python-app.yml

---
+++ test_api.py
@@ -0,0 +1,29 @@
+import fastapi
+from fastapi import FastAPI
+from fastapi.testclient import TestClient
+from main import app
+
+testing_client = TestClient(app)
+
+
+def test_health():
+    response = testing_client.get("/")
+    assert response.status_code == 200
+    assert response.json() == 'OK'
+
+
+def test_matches():
+    response_schedule = testing_client.get('/matches/schedule')
+    response_finished = testing_client.get('/matches/results')
+    assert response_schedule.status_code == 200
+    assert response_finished.status_code == 200
+    print(response_schedule)
+    print(response_finished)
+
+def test_tournaments():
+    response = testing_client.get('/events')
+    assert response.status_code == 200
+    print(response)
+
+
+
